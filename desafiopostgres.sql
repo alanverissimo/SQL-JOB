baf860e15f53fca760aae84ea544a4b073e19b18
Alan Verissimo <alan_verissimo@hotmail.com>
2022-06-09 13:32:51 -0300

    Done!

--- desafiopostgres.sql
+++ desafiopostgres.sql
@@ -1,70 +1,81 @@
-CREATE TABLE Livros
-(
-    L_id SERIAL PRIMARY KEY,
-    nome  VARCHAR(255),
-    ano DATE,
-    reserva INTEGER,
-    prateleira INTEGER
+CREATE TABLE livros (
+    livro_id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    ano INTEGER,
+    maximo_tempo_reserva INTEGER,
+    prateleira INTEGER,
 );
-    INSERT INTO Livros (nome, ano, reserva, prateleira) VALUES('Senhor dos Aneis','2021-06-01', 15, 2);
-    INSERT INTO Livros (nome, ano, reserva, prateleira) VALUES('Harry Potter e a Pedra Fiolosofal','2012-01-11',12, 1);
-    INSERT INTO Livros (nome, ano, reserva, prateleira) VALUES('O Hobbit','2002-06-21',15, 2);
-    INSERT INTO Livros (nome, ano, reserva, prateleira) VALUES('Duna,'2005-011-05',10, 2);
+
+INSERT INTO livros (nome, ano, maximo_tempo_reserva, prateleira) VALUES ('Senhor dos Anéis', 2001, 11, 23);
+INSERT INTO livros (nome, ano, maximo_tempo_reserva, prateleira) VALUES ('Harry Potter e a Pedra Filosofal', 1999, 5, 16);
+INSERT INTO livros (nome, ano, maximo_tempo_reserva, prateleira) VALUES ('O Hobbit', 2004, 16, 23);
+INSERT INTO livros (nome, ano, maximo_tempo_reserva, prateleira) VALUES ('Duna', 2003, 1, 25);
     
-    SELECT * FROM Livros ORDER BY reserva DESC;
-	
-    SELECT L_id, nome, ano, reserva, prateleira FROM Livros livro, (SELECT AVG(reserva) media, prateleira prateleiramedia FROM Livros 
-    GROUP BY prateleira) mediareserva WHERE livro.prateleira = mediareserva.prateleiramedia AND livro.reserva >= mediareserva.media
-															   
-	---------------------------------------------------------------------------------------------
-	CREATE TABLE Mentores
-(
-    Mnt_id SERIAL PRIMARY KEY,
-    nome  VARCHAR(255),
-	turma VARCHAR(255)
-);
-    INSERT INTO Mentores(nome, turma) VALUES ('Paulo, 'Vertigo');
-    INSERT INTO Mentores(nome, turma) VALUES ('Jorge', 'Vertigo');
-    INSERT INTO Mentores(nome, turma) VALUES ('Pedro', 'Five Academy');  
-	
-    SELECT * FROM Mentores			
-															   
-	---------------------------------------------------------------------------------------------
-															   
- CREATE TABLE Postagens(
-    Post_id SERIAL PRIMARY KEY,
-    Mnt_id SERIAL,
-    texto VARCHAR(255), 
-    FOREIGN KEY (Mnt_id) REFERENCES Mentores(Mnt_id)
-);
-    INSERT INTO Postagens(Mnt_id, texto) VALUES (1, 'Java');
-    INSERT INTO Postagens(Mnt_id, texto) VALUES (2, 'JavaScript');
-    INSERT INTO Postagens(Mnt_id, texto) VALUES (3, 'Node.JS');
-    INSERT INTO Postagens(Mnt_id, texto) VALUES (4, 'PostgreSQL');
-	
-    SELECT * FROM Postagens
-															   
-	---------------------------------------------------------------------------------------------
-															   
-	CREATE TABLE Curtidas(
-    M_id SERIAL,
-    P_id SERIAL,
-	curtidas INTEGER,
-    FOREIGN KEY (Mnt_id) REFERENCES Mentores(Mnt_id),
-    FOREIGN KEY (Post_id) REFERENCES Postagens(Post_id)
-);
-    INSERT INTO Curtidas(Mnt_id, Post_id, curtidas) VALUES (1,4,10);
-    INSERT INTO Curtidas(Mnt_id, Post_id, curtidas) VALUES (2,3,7);
-    INSERT INTO Curtidas(Mnt_id, Post_id, curtidas) VALUES (3,2,15);
-    INSERT INTO Curtidas(Mnt_id, Post_id, curtidas) VALUES (4,1,3);
-	
-    SELECT * FROM Curtidas
-															   
-	SELECT nome, curtidas FROM Mentores, Curtidas WHERE Mentores.Mnt_id = Curtidas.Mnt_id GROUP BY(nome, curtidas) ORDER BY Curtidas DESC
-	
-    SELECT L_id, nome, ano, reserva, prateleira FROM Livros livro, (SELECT AVG(reserva) media, prateleira prateleiramedia FROM Livros 
-    GROUP BY prateleira) mediareserva WHERE livro.prateleira = mediareserva.prateleiramedia AND livro.reserva >= mediareserva.media
+SELECT livro_id, nome, maximo_tempo_reserva 
+FROM livros
+WHERE maximo_tempo_reserva > (
+    SELECT AVG(maximo_tempo_reserva)::NUMERIC(12,2) as media_de_tempo 
+    FROM livros 
+    GROUP BY prateleira
+    LIMIT 1
+) ORDER BY livro_id;
+
+---------------------------------------------------------------------------------------------------
+
+CREATE TABLE posts (
+    post_id SERIAL PRIMARY KEY,
+    mnt_id SERIAL,
+    descricao VARCHAR(255),
     
-    SELECT turma, Post_id FROM Mentores INNER JOIN Curtidas ON Mentores.Mnt_id = Curtidas.Mnt_id ORDER BY turma;
+    FOREIGN KEY (mnt_id) REFERENCES mentores (mnt_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE curtidas (
+    mnt_id SERIAL,
+    post_id SERIAL,
     
-    SELECT turma, ROUND( AVG(P_id)::numeric, 2 ) AS media FROM Mentores INNER JOIN Curtidas ON Mentores.Mnt_id = Curtidas.Mnt_id GROUP BY(turma);
+    PRIMARY KEY (mnt_id, post_id),
+    FOREIGN KEY (mnt_id) REFERENCES mentores (mnt_id),
+    FOREIGN KEY (post_id) REFERENCES posts (post_id)
+);
+
+INSERT INTO mentores (nome, sala_de_aula) VALUES ('Warren Buffett', 'Sala de Aula Torvalds');
+INSERT INTO mentores (nome, sala_de_aula) VALUES ('Steven Spielberg', 'Sala de Aula Gates');
+INSERT INTO mentores (nome, sala_de_aula) VALUES ('Socrates', 'Sala de Aula Jobs');
+
+INSERT INTO posts (mnt_id, descricao) VALUES (1, 'Post 01');
+INSERT INTO posts (mnt_id, descricao) VALUES (1, 'Post 02');
+INSERT INTO posts (mnt_id, descricao) VALUES (3, 'Post 03');
+
+INSERT INTO curtidas (mnt_id, post_id) VALUES (1, 1);
+INSERT INTO curtidas (mnt_id, post_id) VALUES (3, 2);
+INSERT INTO curtidas (mnt_id, post_id) VALUES (3, 3);
+
+-----------------------------------------------------------------
+
+SELECT mentores.nome, COUNT(curtidas.mnt_id) AS curtidas 
+FROM mentores 
+INNER JOIN curtidas ON curtidas.mnt_id = mentores.mnt_id 
+GROUP BY 1 
+ORDER BY 1
+
+--------------------------------------------------------------
+
+SELECT mentores.sala_de_aula, curtidas.post_id 
+FROM mentores 
+INNER JOIN curtidas ON curtidas.mnt_id = mentores.mnt_id 
+GROUP BY 1, 2 
+ORDER BY 2
+
+------------------------------------------------------------------
+
+SELECT mentores.sala_de_aula, AVG(curtidas.post_id)::NUMERIC(11,1) as media
+FROM mentores 
+INNER JOIN curtidas ON curtidas.mnt_id = mentores.mnt_id 
+GROUP BY mentores.sala_de_aula										   
+
+-----------------------------------------------------------------------
+	
+	
